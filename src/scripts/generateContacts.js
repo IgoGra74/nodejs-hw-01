6ff75577cc16f5ea4dc751c4f7a5362509fd6f95
IgorGra1 <gis1@ukr.net>
2024-05-16 22:13:35 +0300

    update 2

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,39 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  let existingContacts;
+  try {
+    existingContacts = JSON.parse(await fs.readFile(PATH_DB));
+  } catch (error) {
+    console.error(
+      `An error occurred while reading data from the file ${PATH_DB}:`,
+      error,
+    );
+    return;
+  }
 
-await generateContacts(5);
+  const newContacts = [];
+
+  for (let i = 0; i < number; i++) {
+    newContacts.push(createFakeContact());
+  }
+
+  const updatedContacts = existingContacts
+    ? existingContacts.concat(newContacts)
+    : newContacts;
+
+  await writeDataToFileAsync(PATH_DB, updatedContacts);
+};
+
+const writeDataToFileAsync = async (filePath, data) => {
+  try {
+    await fs.writeFile(filePath, JSON.stringify(data, null, 2));
+    console.log(`The data was successfully written to the file ${filePath}`);
+  } catch (error) {
+    console.error(`Error writing data to file ${filePath}:`, error);
+  }
+};
+
+await generateContacts(1);
