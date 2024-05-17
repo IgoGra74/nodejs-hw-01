da03fa7743d4eab1173e20caa38feabd853d0b13
IgorGra1 <gis1@ukr.net>
2024-05-17 21:44:10 +0300

    update

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -4,7 +4,7 @@ import { PATH_DB } from '../constants/contacts.js';
 export const thanos = async () => {
   try {
     const contacts = JSON.parse(await fs.readFile(PATH_DB));
-    const updatedContacts = contacts.filter(() => Math.random() < 0.5);
+    const updatedContacts = contacts.filter(() => Math.random() >= 0.5);
     await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
   } catch (error) {
     console.error(`Error writing data to file ${PATH_DB}:`, error);
